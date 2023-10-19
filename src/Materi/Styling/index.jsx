e7d0a738d8fee7aba9777f69aaee2752ad3da956
Nurhalim Habibie <79140045+nurhalimhabibie@users.noreply.github.com>
2023-10-19 17:29:41 +0700

    used sass

--- src/Materi/Styling/index.jsx
+++ src/Materi/Styling/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Plain from "./Plain";
+import Sass from "./Sass";
 
 export default class Styling extends React.Component {
   render() {
     return (
       <div>
-        <Plain />
+        <Sass />
       </div>
     );
   }
